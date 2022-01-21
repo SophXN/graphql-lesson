4cae8af83d284e9b2da17c88425a8acb1711be61
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2022-01-21 13:57:02 +0000

    Resolvers for graphql

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,3 +1,14 @@
 // Object passed to client that lets it know what values to resolve that is called to the local client side
 import {gql} from 'apollo-boost';
 
+export const typeDefs = gql`
+extends type Mutation {
+  ToggleCartHidden: Boolean!
+}
+`
+
+export const resolvers = {
+  Mutation: {
+    toggleCartHidden: (_root, _args, _context, _info) =>
+  }
+}
